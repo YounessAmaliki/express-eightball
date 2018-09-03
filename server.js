751e26587bf9897ac554a5cba4e4116979da1895
Amaliki Youness <youness.amaliki@student.ap.be>
2018-09-03 16:05:39 +0200

    LocalStorage module
    Localstorage module toevoegen

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const localStorage = require('node-localStorage');
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port 3000');
@@ -21,6 +22,17 @@ app.get('/ask', (req, res) => {
    res.render('ask.ejs', {})
 })
 
+app.post('/ask', (req, res) => {
+ var query = { name: req.body.name }
+if (typeof localStorage === "undefined" || localStorage === null) {
+  var LocalStorage = require('node-localstorage').LocalStorage;
+  localStorage = new LocalStorage('./scratch');
+}
+ 
+localStorage.setItem('myFirstKey', 'myFirstValue');
+console.log(localStorage.getItem('myFirstKey'));
+       res.render('answer.ejs', { product: result[0] })
 
+})
 
 
