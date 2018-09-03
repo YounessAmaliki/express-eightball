f081a13864432140a82d6d3baa2c66555622df6a
Amaliki Youness <youness.amaliki@student.ap.be>
2018-09-03 16:43:29 +0200

    Antwoorden
    Antwoorden proberen op te halen

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const localStorage = require('node-localStorage');
+/*const localStorage = require('node-localStorage');*/
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port 3000');
 });
+
+if (typeof localStorage === "undefined" || localStorage === null) {
+  var LocalStorage = require('node-localstorage').LocalStorage;
+  localStorage = new LocalStorage('./scratch');
+}
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -20,18 +25,18 @@ app.get('/', (req, res) => {
 
 app.get('/ask', (req, res) => {
    res.render('ask.ejs', {})
+   console.log(localStorage.getItem('answers'));
 })
 
 app.post('/ask', (req, res) => {
- var query = { name: req.body.name }
-if (typeof localStorage === "undefined" || localStorage === null) {
-  var LocalStorage = require('node-localstorage').LocalStorage;
-  localStorage = new LocalStorage('./scratch');
-}
- 
-localStorage.setItem('myFirstKey', 'myFirstValue');
-console.log(localStorage.getItem('myFirstKey'));
-       res.render('answer.ejs', { product: result[0] })
+var question = { question: req.body.question }
+if (localStorage.getItem(question) =! null) return localStorage.getItem(question);
+  else 
+  localStorage.setItem('answers', 'It is certain','It is decidedly so','Without a doubt','Yes definitely','You may rely on it','As I see it, yes','Most likely');
+  var answer = localStorage.getItem('answers');
+  localStorage.getItem(answer,question);
+  res.render('answer.ejs', { question: answer[0] })
+
 
 })
 
